98f3e3a08bd3c70ec2a1497e46d23e0ea95e9ddf
vivian-alemba <alembavivianauma@gmail.com>
2020-09-16 11:50:51 +0300

    updated code

---
+++ script.js
@@ -0,0 +1,21 @@
+function addNumbers(num){
+    return num+num
+
+}
+console.log(addNumbers(12))
+
+function multiplyNumber(){
+
+}
+
+function divideNumber(){
+
+}
+
+function substractNumber(){
+
+}
+
+function clearInput(){
+
+}
